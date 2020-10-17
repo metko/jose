e85dbebad6225ea7f7ced490493f9542a5d0ba6b
Thomas Moiluiavon <thomas.m@thewalkingnerds.com>
2020-10-17 12:00:55 +0200

    Config object refactored

--- src/Utils/Config.php
+++ src/Utils/Config.php
@@ -3,50 +3,127 @@
 namespace Jose\Utils;
 
 use ErrorException;
-use Jose\Core\Exceptions\MissingKeyExeption;
-use Symfony\Component\Filesystem\Exception\FileNotFoundException;
 
-class Config {
-    
+class Config
+{
+
+    /**
+     * $instance  \Jose\Utils\Config
+    */
     public static $instance = null;
 
+    /**
+     * $config array
+     * 
+     * The config object
+    */
     public $config = [];
 
+     /**
+     * $configuration string|array
+     * 
+     * The custom config pass
+    */
+    public $configuration = null;
+    
+    /**
+     * $finder \Jose\Utils\Finder
+     *
+     * The finder object
+     */
     public $finder = null;
-
-    public function __construct() {
+    
+    /**
+     * __construct
+     *
+     * @return void
+     */
+    public function __construct()
+    {
         $this->finder = Finder::getInstance();
     }
-
-    // ********************************
-    // Get file and/with key in array
-    public function get($key = null) {
-
-        if(!$key) {
+    
+    /**
+     * Get conf array by key / or all the conf
+     *
+     * @param  string $key
+     * @return array|null
+     */
+    public function get($key = null): ?array
+    {
+        if ($key) {
+            if (array_key_exists($key, $this->config)) {
+                return $this->config[$key];
+            }
+            return null;
+        } else {
             return $this->config;
         }
-     
-        if( ! array_key_exists($key , $this->config ))  {
-            throw new MissingKeyExeption($key);
-        }
-        
-        return $this->config[$key];
-
-    } 
-    
-    public function getContext() {
-        return $this->finder->require(APP.'context.php');
     }
-
-    public static function getInstance() {
-        if(!self::$instance) {
+ 
+    /**
+     * Get config instance object
+     *
+     * @return void
+     */
+    public static function getInstance(): \Jose\Utils\Config
+    {
+        if (!self::$instance) {
             self::$instance = new Config();
         }
         return self::$instance;
-    } 
-
-    public function init() {
-        $this->config = $this->finder->require(APP.'config.php');
     }
 
+    /**
+     * Set a configuration array, or file path
+     *
+     * @param  string|array $config
+     * @return void
+     */
+    public function define($config): void
+    {
+        $this->configuration = $config;
+    }
+
+    /**
+     * Init the configuration object of the app
+     * 
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        // Rirst, check if conf_path is defined
+        if ($this->configuration) {
+
+            // if it's a string, check if the file exist
+            if (is_string($this->configuration)) {
+
+                // if defined file doest exists, throw an error
+                if (!$this->finder->file_exists(ROOT . $this->configuration)) {
+                    throw new ErrorException(ROOT . $this->configuration . " doesnt exists");
+                }
+
+                // require the desired file
+                $this->configuration = $this->finder->require(ROOT . $this->configuration);
+
+            } else if (!is_array($this->configuration)) {
+                throw new ErrorException('the config object must be an array or a file path.');
+            }
+            
+        } else {
+
+            // If not, check if the default path exist in the client app
+            if ($this->finder->file_exists(APP . 'config.php')) {
+                $this->configuration =  $this->finder->require(APP . 'config.php');
+            }
+        }
+
+        // Then merge the default conf, with the new conf if exist
+        $this->config = array_merge(
+            $this->finder->require(ROOT_JOSE . 'config_default.php'),
+            $this->configuration ?? []
+        );
+
+    }
 }
