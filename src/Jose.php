d1810f1d2d46d7066b22ade6e022d13a80fb73e1
Thomas Moiluiavon <thomas.m@thewalkingnerds.com>
2020-10-17 12:01:41 +0200

    Add check requiremnent methods

--- src/Jose.php
+++ src/Jose.php
@@ -4,6 +4,7 @@ namespace Jose;
 
 use ErrorException;
 use Jose\Core\Exceptions\DependencyMissingException;
+use Jose\Utils\Config;
 use Timber\Timber;
 
 class Jose {
@@ -15,16 +16,43 @@ class Jose {
     private static $instance = null;
 
     /**
-     * Get  the instance og the site
+     * Get  the instance of the global app
      *
      */
-    public static function app()
+    public static function app(): \Jose\Core\App
     {
         if( ! self::$instance) {
             // need to check
-            self::$instance = new \Jose\Core\App();
+            if(self::checkRequirments() ) {
+                self::$instance = new \Jose\Core\App();
+            }
+            
         }
         return self::$instance;
+    }
+
+    public static function config($config)
+    {
+        return Config::getInstance()->define($config);
+    }
+
+    public static function get_config($key = null)
+    {
+        return Config::getInstance()->get($key);
+    }
+    
+
+    /**
+     * Check all the necessary php version, lib etc here....
+     *
+     * @return bool
+     */
+    private static function checkRequirments(): ?bool
+    {
+        if ( ! class_exists('\WP') ) {
+            throw new ErrorException('You need wordpress to use Jose. Sorry...');
+        }
+        return true;
     }
 
 }
