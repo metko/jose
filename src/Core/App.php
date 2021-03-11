21b31c8ae38c6d6f1cc1262d7635e0434167d87f
Thomas M <thomas.m@thewalkingnerds.com>
2021-03-11 19:31:00 +0100

    update: auto resolution for template

--- src/Core/App.php
+++ src/Core/App.php
@@ -74,10 +74,63 @@ class App  {
      * @param array $context
      * @return void
      */
-    public function render(String $template, $context = [])
+    public function render(String $template = null, $context = [])
     {
         $context = array_merge( $this->beforeRender(), $context);
-        return Timber::render($template.'.twig', $context);
+
+        // if i'ts a page
+        // dd(is_page());
+
+        // if i's a single
+//        dd(is_single());
+
+       //  dd(is_singular());
+
+        //dd(get_page_template_slug());
+        if(! $template) {
+            $template = $this->getTemplate($context);
+        }
+
+
+
+        $arrayTemplate = array_map([$this, 'add_extension'], is_array($template) ? $template: [$template]);
+        return Timber::render($arrayTemplate, $context);
+    }
+
+    /**
+     * @param $string
+     * @return string
+     */
+    private function add_extension($string): string
+    {
+        return $string . '.twig';
+    }
+
+    private function getTemplate($context) {
+
+        // If the post have a page template set in admin, use it
+        if(get_page_template_slug()) {
+            // we juste remove the extension since timber need to call a twig
+            return str_replace( '.php', '', get_page_template_slug());
+        }
+
+        if(is_page() && is_singular()) {
+            return [
+                'page-'.$context['post']->slug,
+                'page-'.$context['post']->ID,
+                'page'];
+        }
+
+        // if it's archive post type
+        if(is_archive() || is_post_type_archive() ) {
+            return [
+                'archive-'.get_post_type(),
+                'archive'
+            ];
+        }
+
+        return 'something...';
+
     }
 
 
